85cc7d538429d3490697bae805650b79dead6e8c
Drew Beckmen <drew.beckmen@yale.edu>
2020-08-31 20:35:04 -0400

    update dependencies

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ append :linked_files, "config/database.yml"
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
-
+after "deploy", "deploy:migrate"
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
