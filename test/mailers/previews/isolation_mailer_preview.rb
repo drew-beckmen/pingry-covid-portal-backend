faa11dc4ef9b2232a2472d961859490ba0f4273b
Drew Beckmen <drew.beckmen@yale.edu>
2021-02-04 22:26:01 -0500

    update email language and dates to reflect changes

--- test/mailers/previews/isolation_mailer_preview.rb
+++ test/mailers/previews/isolation_mailer_preview.rb
@@ -3,49 +3,49 @@ require 'date'
 class IsolationMailerPreview < ActionMailer::Preview
 
     def isolation_started_email_student
-        isolation = Isolation.new(start_isolation: Date.today, confirmed: true, completed: false, student_id: 1)
+        isolation = Isolation.new(start_isolation: Date.today, confirmed: true, completed: false, student_id: 1565)
         currentStudent = Student.find(isolation.student_id)
         IsolationMailer.with(student: currentStudent, isolation: isolation).isolation_started_email_student
     end 
 
     def isolation_started_email_adult 
-        isolation = Isolation.new(start_isolation: Date.today, confirmed: true, end_date: Date.today + 10, completed: false, student_id: 1)
+        isolation = Isolation.new(start_isolation: Date.today, confirmed: true, end_date: Date.today + 10, completed: false, student_id: 1565)
         currentStudent = Student.find(isolation.student_id)
         IsolationMailer.with(student: currentStudent, isolation: isolation).isolation_started_email_adult
     end 
 
     def isolation_updated_email_student
-        isolation = Isolation.new(start_isolation: Date.today - 10, confirmed: true, date_improving: Date.today - 10, end_date: Date.today, completed: true, student_id: 1)
+        isolation = Isolation.new(start_isolation: Date.today - 10, confirmed: true, date_improving: Date.today - 10, end_date: Date.today, completed: true, student_id: 1565)
         currentStudent = Student.find(isolation.student_id)
         IsolationMailer.with(student: currentStudent, isolation: isolation).isolation_updated_email_student
     end 
 
     def isolation_updated_email_adult
-        isolation = Isolation.new(start_isolation: Date.today - 10, confirmed: true, date_improving: Date.today - 10, end_date: Date.today, completed: true, student_id: 1)
+        isolation = Isolation.new(start_isolation: Date.today - 10, confirmed: true, date_improving: Date.today - 10, end_date: Date.today, completed: true, student_id: 1565)
         currentStudent = Student.find(isolation.student_id)
         IsolationMailer.with(student: currentStudent, isolation: isolation).isolation_updated_email_adult
     end 
 
     def isolation_completed_email_student
-        isolation = Isolation.new(start_isolation: Date.today - 10, confirmed: true, date_improving: Date.today - 10, end_date: Date.today, completed: true, student_id: 1)
+        isolation = Isolation.new(start_isolation: Date.today - 10, confirmed: true, date_improving: Date.today - 10, end_date: Date.today, completed: true, student_id: 1565)
         currentStudent = Student.find(isolation.student_id)
         IsolationMailer.with(student: currentStudent, isolation: isolation).isolation_completed_email_student
     end 
 
     def isolation_completed_email_adult
-        isolation = Isolation.new(start_isolation: Date.today - 10, confirmed: true, date_improving: Date.today - 10, end_date: Date.today, completed: true, student_id: 1)
+        isolation = Isolation.new(start_isolation: Date.today - 10, confirmed: true, date_improving: Date.today - 10, end_date: Date.today, completed: true, student_id: 1565)
         currentStudent = Student.find(isolation.student_id)
         IsolationMailer.with(student: currentStudent, isolation: isolation).isolation_completed_email_adult
     end 
 
     def potential_isolation_started_email_adult
-        isolation = Isolation.new(start_isolation: Date.today - 10, barcode: "ABCDEF", potential: true, date_improving: Date.today - 10, end_date: Date.today, completed: true, student_id: 1)
+        isolation = Isolation.new(start_isolation: Date.today - 10, barcode: "ABCDEF", potential: true, date_improving: Date.today - 10, end_date: Date.today, completed: true, student_id: 1565)
         currentStudent = Student.find(isolation.student_id)
         IsolationMailer.with(student: currentStudent, isolation: isolation).potential_isolation_started_email_adult
     end 
 
     def potential_isolation_started_email_student
-        isolation = Isolation.new(start_isolation: Date.today - 10, barcode: "ABCDEF", potential: true, date_improving: Date.today - 10, end_date: Date.today, completed: true, student_id: 1)
+        isolation = Isolation.new(start_isolation: Date.today - 10, barcode: "ABCDEF", potential: true, date_improving: Date.today - 10, end_date: Date.today, completed: true, student_id: 1565)
         currentStudent = Student.find(isolation.student_id)
         IsolationMailer.with(student: currentStudent, isolation: isolation).potential_isolation_started_email_student
     end 
--- test/mailers/previews/quarantine_mailer_preview.rb
+++ test/mailers/previews/quarantine_mailer_preview.rb
@@ -2,61 +2,61 @@
 require 'date'
 class QuarantineMailerPreview < ActionMailer::Preview
     def quarantine_started_email_student
-        quarantine = Quarantine.new(exposure: Date.today, completed: false, student_id: 1, end_date: Date.today + 20)
+        quarantine = Quarantine.new(exposure: Date.today, completed: false, student_id: 1565, end_date: Date.today + 20)
         currentStudent = Student.find(quarantine.student_id)
         QuarantineMailer.with(student: currentStudent, quarantine: quarantine).quarantine_started_email_student 
     end 
 
     def quarantine_started_email_adult
-        quarantine = Quarantine.new(exposure: Date.today, completed: false, student_id: 1)
+        quarantine = Quarantine.new(exposure: Date.today, completed: false, student_id: 1565)
         currentStudent = Student.find(quarantine.student_id)
         QuarantineMailer.with(student: currentStudent, quarantine: quarantine).quarantine_started_email_adult 
     end 
 
     def quarantine_updated_email_student
-        quarantine = Quarantine.new(exposure: Date.today, completed: false, student_id: 1)
+        quarantine = Quarantine.new(exposure: Date.today, completed: false, student_id: 1565)
         currentStudent = Student.find(quarantine.student_id)
         QuarantineMailer.with(student: currentStudent, quarantine: quarantine).quarantine_updated_email_student
     end 
 
     def quarantine_updated_email_adult
-        quarantine = Quarantine.new(exposure: Date.today, completed: false, student_id: 1)
+        quarantine = Quarantine.new(exposure: Date.today, completed: false, student_id: 1565)
         currentStudent = Student.find(quarantine.student_id)
         QuarantineMailer.with(student: currentStudent, quarantine: quarantine).quarantine_updated_email_adult
     end 
 
     def quarantine_converted_email_student
-        quarantine = Quarantine.new(exposure: Date.today, completed: false, student_id: 1)
+        quarantine = Quarantine.new(exposure: Date.today, completed: false, student_id: 1565)
         currentStudent = Student.find(quarantine.student_id)
         QuarantineMailer.with(student: currentStudent, quarantine: quarantine).quarantine_converted_email_student
     end 
 
     def quarantine_converted_email_adult
-        quarantine = Quarantine.new(exposure: Date.today, completed: false, student_id: 1)
+        quarantine = Quarantine.new(exposure: Date.today, completed: false, student_id: 1565)
         currentStudent = Student.find(quarantine.student_id)
         QuarantineMailer.with(student: currentStudent, quarantine: quarantine).quarantine_converted_email_adult
     end 
 
     def quarantine_completed_email_student
-        quarantine = Quarantine.new(exposure: Date.today - 14, completed: false, student_id: 1)
+        quarantine = Quarantine.new(exposure: Date.today - 14, completed: false, student_id: 1565)
         currentStudent = Student.find(quarantine.student_id)
         QuarantineMailer.with(student: currentStudent, quarantine: quarantine).quarantine_completed_email_student
     end 
 
     def quarantine_completed_email_adult 
-        quarantine = Quarantine.new(exposure: Date.today, completed: false, student_id: 1)
+        quarantine = Quarantine.new(exposure: Date.today, completed: false, student_id: 1565)
         currentStudent = Student.find(quarantine.student_id)
         QuarantineMailer.with(student: currentStudent, quarantine: quarantine).quarantine_completed_email_adult
     end 
 
     def quarantine_testing_email_student
-        quarantine = Quarantine.new(exposure: Date.today - 14, completed: false, student_id: 1)
+        quarantine = Quarantine.new(exposure: Date.today - 14, completed: false, student_id: 1565)
         currentStudent = Student.find(quarantine.student_id)
         QuarantineMailer.with(student: currentStudent, quarantine: quarantine).quarantine_testing_email_student
     end 
 
     def quarantine_testing_email_adult 
-        quarantine = Quarantine.new(exposure: Date.today, completed: false, student_id: 1)
+        quarantine = Quarantine.new(exposure: Date.today, completed: false, student_id: 1565)
         currentStudent = Student.find(quarantine.student_id)
         QuarantineMailer.with(student: currentStudent, quarantine: quarantine).quarantine_testing_email_adult
     end 
