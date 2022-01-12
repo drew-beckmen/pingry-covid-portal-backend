972528bc964fcdc4a11d47497dd15c5211c2259c
Drew Beckmen <drew.beckmen@yale.edu>
2022-01-11 21:45:19 -0500

    remove symptom improving from serializer

--- app/controllers/api/v1/exports_controller.rb
+++ app/controllers/api/v1/exports_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::ExportsController < ApplicationController
     
     def data_export
         DataExport.generate_csv
-        ExportMailer.with(user: currentUser).send_csv_data_export.deliver_now
+        ExportMailer.with(user: current_user).send_csv_data_export.deliver_now
+        render json: {}, status: 200
     end
 end
